4755c54f4a7672fe377fdd606b3c885e5b5f124d
Amado Martinez <amado@projectivemotion.com>
2016-07-26 06:33:08 -0500

    Initial Commit

---
+++ build.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Project: amadomartinez.mx
+ *
+ * @author Amado Martinez <amado@projectivemotion.com>
+ */
+
+$feed   =   'http://projectivemotion.com/feed/';
+
+$feedrc =   simplexml_load_file($feed);
+
+echo $feedrc->channel->item[0]->title;
+
+$items  =   [];
+foreach($feedrc->channel->item as $item):
+    $items[]    =   sprintf("<li><a href=\"%s\">%s</a> » <i><span>%s</span></i></li>",
+            $item->link,
+            $item->title,
+            $item->pubDate
+            );
+endforeach;
+
+$inout  =   ['index.source.html' => 'index.html'];
+
+foreach($inout as $in => $out)
+{
+    $template   =   file_get_contents($in);
+    $rendered   =   preg_replace('/{posts}/', implode("\n", $items), $template);
+    file_put_contents($out, $rendered);
+}
+
+
+
