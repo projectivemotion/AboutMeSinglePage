0c4179fef3142390c949d9c6d224422c83425297
Amado Martinez <amado@projectivemotion.com>
2016-09-02 07:49:02 -0500

    Expanded support for configuration.

--- config.php
+++ config.php
@@ -17,8 +17,26 @@ return [
     'linkedin'  =>  'https://mx.linkedin.com/in/amado-martinez-142a6613',
     'stackexchange' =>  'https://stackoverflow.com/users/2723108/amado-martinez',
 
+    // meta
+    'keywords'  =>  'Freelance Web Developer, LAMP Developer, Freelancer Mexico, Web Developer Mexico',
+    'description'   =>  'I am a web developer working remotely from Mexico.',
+    'og_title'  =>  'Freelance Web Developer',
+    'og_description'    =>  'I develop web applications.',
+    'og_url'    =>  'http://amadomartinez.mx/',
+    'og_image'    =>  'http://amadomartinez.mx/index_files/logo-high-resolution.png',
+    'twitter_user'    =>  '@amadocodes',
+
     // page stuff
     'avatar'   =>  'https://avatars3.githubusercontent.com/u/39117?v=3&s=460',
     'clients'   =>  'http://projectivemotion.com/projects/',
-    'about' =>  'Linux, Apache, Mysql, Php Developer'
+    'about_mini' =>  'Linux, Apache, Mysql, Php Developer',
+    'about_long'    => <<<ndoc
+<p>I am a freelance developer. I work across all timezones! </p>
+
+<p>location: Reynosa, Mexico. </p>
+<p></p>
+
+<p>email: <a href="mailto:"><img style="margin-top:-4px; margin-left:-20px;" height="16px" src="http://projectivemotion.com/files/2009/06/amado-at.png" /> </a></p>
+ndoc
+
 ];
